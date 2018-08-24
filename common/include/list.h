15753f31bdcc2ce547b4ee334c3280b1f8c0e20b
Shen <shendubo@me.com>
2018-08-24 23:33:44 +0800

    list file
    add function

--- common/include/list.h
+++ common/include/list.h
@@ -1,7 +1,7 @@
 #ifndef _LIST_H
 #define _LIST_H
 
-struct list_head{
+typedef struct list_head{
     struct list_head *next;
     struct list_head *prev;
 
@@ -15,42 +15,91 @@ struct list_head{
 
 
 #define INIT_LIST_HEAD(ptr) do { \
-    (ptr)->next = (ptr); \
-    (ptr)->prev = (ptr); \
+    (ptr)->next = (ptr); (ptr)->prev = (ptr); \
 }while(0)
 
 
-static __inline__ void __list_add(LIST_HEAD_ST *new, 
+static __inline__ void __list_add(LIST_HEAD_ST *entry,
                                   LIST_HEAD_ST *prev,
                                   LIST_HEAD_ST *next)
 {
-    next->prev = new;
-    new->next = next;
-    new->prev = prev;
-    prev->next = new;
+    next->prev = entry;
+    entry->next = next;
+    entry->prev = prev;
+    prev->next = entry;
+
+    return;
 }
 
 
-void list_add(LIST_HEAD_ST *new, LIST_HEAD_ST *head)
+void list_add(LIST_HEAD_ST *entry, LIST_HEAD_ST *head)
 {
-    __list_add(new, head,head->next);
+    __list_add(entry, head,head->next);
+
+    return;
 }
 
-void list_add_tail(LIST_HEAD_ST *new, LIST_HEAD_ST *head)
+void list_add_tail(LIST_HEAD_ST *entry, LIST_HEAD_ST *head)
 {
-    __list_add(new,head->prev,head);
+    __list_add(entry,head->prev,head);
+
+    return;
 }
 
 static __inline__ void __list_del(LIST_HEAD_ST *prev, LIST_HEAD_ST *next)
 {
     next->prev = prev;
-    prev->next = next; 
+    prev->next = next;
+    
+    return;
 }
 
 void list_del(LIST_HEAD_ST *entry)
 {
     __list_del(entry->prev, entry->next);
+
+    return;
 }
+
+static __inline__ void list_del_init(LIST_HEAD_ST *entry)
+{
+    __list_del(entry->prev, entry->prev);
+    INIT_LIST_HEAD(entry);
+
+    return;
+}
+
+
+static __inline__ int list_empty(LIST_HEAD_ST *head)
+{
+    return head->next == head;
+}
+
+static __inline__ void list_splice(LIST_HEAD_ST *list, LIST_HEAD_ST *head)
+{
+    LIST_HEAD_ST *first = list->next;
+    LIST_HEAD_ST *last = list->prev;
+    LIST_HEAD_ST *at = head->next;
+
+    if (first != list)
+    {
+        first->prev = head;
+        head->next = first;
+
+        last->next = at;
+        at->prev = last;
+    }
+
+    return;
+}
+
+
+#define list_entry(ptr, type, member) \
+    ((type *)((char *)(ptr)-(unsigned long)(&((type *)0)->member)))
+
+
+#define list_for_each(pos, head) \
+    for (pos = (head)->next; pos != (head); pos = pos->next)
 
 
 #endif
